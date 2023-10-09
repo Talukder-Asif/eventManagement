7f41547e5f5a37916a83ca57beff4dd83b9b32a4
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2023-10-09 15:39:27 +0600

    Private route done

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import { AuthContex } from "../Provaider/AuthProvider";
-import Login from "../Pages/UserInfo/Login";
+import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({children}) => {
     const {user} = useContext(AuthContex)
     if(user){
-        return {children}
+        return children
     }
-    return <Login></Login>
+    return <Navigate to="/signin"></Navigate>
      
 };
 
